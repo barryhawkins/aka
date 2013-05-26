0784a450291b3fc4992ea5482eb827bc4c64c1a5
Barry Hawkins <barry@barryhawkins.com>
2013-05-25 22:16:05 -0700

    Utility script for rapid deployment in local development

---
+++ dev_deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+export WARCRAFT_ADDON_HOME="/Applications/World of Warcraft/Interface/AddOns"
+export MONIKER_DIRECTORY=${WARCRAFT_ADDON_HOME}/Moniker
+
+if [ ! -d "${MONIKER_DIRECTORY}" ]; then
+  mkdir -v "${MONIKER_DIRECTORY}"
+else
+  echo "'${MONIKER_DIRECTORY}' exists, proceeding..."
+fi
+
+if [ -d "${MONIKER_DIRECTORY}" ]; then
+  cp -v src/Moniker.lua "${MONIKER_DIRECTORY}"
+  cp -v src/Moniker.toc "${MONIKER_DIRECTORY}"
+  cp -v src/Moniker.xml "${MONIKER_DIRECTORY}"
+else
+  echo "'${MONIKER_DIRECTORY}' doesn't seem to exist?"
+fi
